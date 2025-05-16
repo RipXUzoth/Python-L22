595b1a69705b09602bd7af09cd65de0ea523af9e
RipXUzoth <akkuharish1@gmail.com>
2025-05-16 15:26:58 +0530

    gtcntrycd

---
+++ gtcntrycd.py
@@ -0,0 +1,5 @@
+country_code = {'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+print("Country code for india -")
+print(country_code.get('india', 'Not found' ))
+print("Country code for japan -")
+print(country_code.get('japan', 'Not found'))
